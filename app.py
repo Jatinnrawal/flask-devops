dad3a1ec966f517534e287097e650b62eb042c82
Jatin rawal <rawaljatin@gmail.com>
2026-07-11 18:09:23 +0530

    test webhook 3 trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DevOps demo - v2!, wassup Jatin rawal "
+    return "Hello from DevOps demo - v2!, wassup Jatin rawal how u doin"
 
 @app.route('/health')
 def health():
